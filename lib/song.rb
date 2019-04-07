19271963086a325a5cf8fe527c991050eb9092a5
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-04-07 04:58:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,7 +4,9 @@ class Song
   @@count = 0 
   
   def initialize(name, artist, genre)
-    @name = name
+    @name = name 
+    @artist = artist
+    @genre = genre 
     @@count += 1 
     @@genres << :genre 
     @@artists << :artist
